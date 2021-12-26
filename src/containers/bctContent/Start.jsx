8dd166bbdcb6cfff3467a10d988a5777c0e75e59
cindyeme <emereninicynthia@gmail.com>
2021-12-26 20:04:41 +0100

    become-a-teacher modifications/fixes

--- src/containers/bctContent/Start.jsx
+++ src/containers/bctContent/Start.jsx
@@ -3,14 +3,14 @@ import teacher from '../../assets/img/bb-teach.png';
 import Button from '../../components/button/Button';
 
 const Start = () => (
-  <section className='relative'>
-    <div className="grid grid-cols-12 gap-y-5 gap-x-7 items-center">
+  <section className='relative mt-16 lg:mt-0'>
+    <div className="grid grid-cols-12 gap-y-0 gap-x-7 items-center">
       <div className="md:col-span-7 col-span-full mb-10" data-aos="flip-up">
         <div className="flex flex-col space-y-4">
           <h2 className="tracking-wide lg:leading-normal text-3xl md:text-3xl lg:text-4xl font-bolder text-primary-900 text-center lg:text-left">
             Start Your Teaching Career Today With LearnR
           </h2>
-          <p className="leading-relaxed text-center lg:text-left text-sm md:text-base font-medium">
+          <p className="leading-relaxed text-center lg:text-left text-sm md:text-base font-medium px-2">
             We take our mission of increasing global access to quality
             education seriously. We connect learners to the best tutors and
             courses from around the world. The quick, brown fox jumps over a
@@ -26,7 +26,7 @@ const Start = () => (
       </div>
       <div className="md:col-span-5 col-span-full" data-aos="flip-down">
         <div className="img-box">
-          <img src={teacher} alt="teacher" className="img-fluid" />
+          <img src={teacher} alt="teacher" className="img-fluid w-full" />
         </div>
       </div>
     </div>
