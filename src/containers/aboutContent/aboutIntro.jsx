bbe2868d87bef8c9df0bfd711c51297ef7197f38
cindyeme <emereninicynthia@gmail.com>
2022-01-22 15:51:07 +0100

    deployed

--- src/containers/aboutContent/aboutIntro.jsx
+++ src/containers/aboutContent/aboutIntro.jsx
@@ -15,13 +15,14 @@ const AboutIntro = () => (
         <h2 className="tracking-wide lg:leading-normal text-3xl my-5 md:text-3xl lg:text-4xl font-bolder text-primary-900 text-center lg:text-left">
           Education in continuing a proud tradition.
         </h2>
-        <p className="leading-relaxed text-center lg:text-left text-sm md:text-base font-medium">
+        <p className="leading-relaxed text-center lg:text-left text-sm md:text-base">
           LearnR aims to bring together students and teachers on the community
           platform. We take our mission of increasing global access to quality
-          education seriously. We connect learners to the best tutors and
-          courses from around the world. The quick, brown fox jumps over a lazy
-          dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox
-          whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad
+          education seriously. We illuminate academic textbooks with annotations
+          and discussion around content. LearnR mission is to facilitate the
+          easy spread of knowledge within our community/schools. Our goal is to
+          promote open-source academic textbooks and publishing for teachers and
+          learners alike.
         </p>
         <hr className="text-primary-amber w-20 py-4 mt-5" />
         <ul className="list-disc lg:list-inside px-4 lg:px-0 flex flex-col space-y-3 text-sm md:text-base font-medium">
@@ -30,7 +31,7 @@ const AboutIntro = () => (
             We Connect Learners To The Best Teachers From Around The World
           </li>
           <li>Our Mission Is Increasing Global Access To Quality Education</li>
-          <li>129k Online Available Notes</li>
+          <li>129k Online Available Textbooks</li>
         </ul>
         <div className="mt-10">
           <Button className="rounded-full ring ring-primary-900 ring-offset-2 !bg-primary-amber !px-5 !py-4">
